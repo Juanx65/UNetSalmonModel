9aca00eee73137629d2da1378801acd6bcc02f6b
juanx65 <juan.aguileraca@sansano.usm.cl>
2022-07-26 05:00:38 -0400

    borre algo del train.py

--- train.py
+++ train.py
@@ -21,26 +21,6 @@ def train(opt):
     model.fit_generator(myGene,steps_per_epoch=opt.step_per_epoch,epochs=opt.epochs,callbacks=[model_checkpoint])
 
     model.summary()
-    # graficos y weas lindas
-    acc = history.history['accuracy']
-    val_acc = history.history['val_accuracy']
-    loss = history.history['loss']
-    val_loss = history.history['val_loss']
-    epochs_range = range(epochs)
-
-    plt.figure(figsize=(8, 8))
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs_range, acc, label='Training Accuracy')
-    plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
-
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs_range, loss, label='Training Loss')
-    plt.plot(epochs_range, val_loss, label='Validation Loss')
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
-    plt.show()
     
 def parse_opt():
     parser = argparse.ArgumentParser()
